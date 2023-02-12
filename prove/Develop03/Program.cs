1c56052cf0619ed1d5e73033edb75c7b86be8485
DawsonBauman <bau15011@byui.edu>
2023-02-11 19:51:27 -0700

    Finished

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,99 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ScriptureApp
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+            // Create a new scripture
+            Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
+            // Display the scripture
+            Console.WriteLine(scripture);
+            Console.WriteLine("Press enter to hide words or type 'quit' to exit");
+
+            // Loop until all words are hidden or the user types quit
+            while (!scripture.IsHidden)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                // Clear the console screen and hide some words
+                Console.Clear();
+                scripture.HideWords();
+
+                // Display the scripture
+                Console.WriteLine(scripture);
+                Console.WriteLine("Press enter to hide words or type 'quit' to exit");
+            }
+        }
+    }
+
+    class Scripture
+    {
+        private string reference;
+        private string text;
+        private bool[] hiddenWords;
+
+        public Scripture(string reference, string text)
+        {
+            this.reference = reference;
+            this.text = text;
+            this.hiddenWords = new bool[text.Split(' ').Length];
+        }
+
+        public bool IsHidden
+        {
+            get
+            {
+                foreach (bool isHidden in hiddenWords)
+                {
+                    if (!isHidden)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public void HideWords()
+        {
+            Random rand = new Random();
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!hiddenWords[i])
+                {
+                    if (rand.NextDouble() < 0.5)
+                    {
+                        hiddenWords[i] = true;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string[] words = text.Split(' ');
+            string result = reference + ": ";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (hiddenWords[i])
+                {
+                    result += "[...] ";
+                }
+                else
+                {
+                    result += words[i] + " ";
+                }
+            }
+            return result;
+        }
     }
 }
