e6d80af281a2dc22616bd66a1bb4c37cff496041
DawsonBauman <bau15011@byui.edu>
2023-04-04 23:52:50 -0600

    What I have so far as of right now.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,146 @@
 using System;
+using System.Collections.Generic;
+
+// Base Activity class
+class Activity
+{
+    protected DateTime date;
+    protected int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return "";
+    }
+
+    protected string FormatDate(DateTime date)
+    {
+        return date.ToString("dd MMM yyyy");
+    }
+}
+
+// Derived class for Running activity
+class Running : Activity
+{
+    private double distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (distance / minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / distance;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{FormatDate(date)} Running ({minutes} min) - Distance {distance} miles, Speed {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
+
+// Derived class for Cycling activity
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{FormatDate(date)} Cycling ({minutes} min) - Speed {speed} mph";
+    }
+}
+
+// Derived class for Swimming activity
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{FormatDate(date)} Swimming ({minutes} min) - Distance {GetDistance()} miles, Speed {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create activity objects
+        Activity running = new Running(new DateTime(2022, 11, 3), 30, 3.0);
+        Activity cycling = new Cycling(new DateTime(2022, 11, 3), 45, 15.5);
+        Activity swimming = new Swimming(new DateTime(2022, 11, 3), 60, 10);
+
+        // Add activities to a list
+        List<Activity> activities = new List<Activity>();
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        // Display summary for each activity
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
